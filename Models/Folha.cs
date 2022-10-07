562b77d14b15f13ef0122670b62a04634ac3e2ac
FKRIK <fernandokrik@hotmail.com>
2022-10-06 22:14:31 -0300

    if {}

--- Models/Folha.cs
+++ Models/Folha.cs
@@ -21,16 +21,20 @@ namespace API_Folhas.Models
         
         public Folha(Funcionario funcionario){
             // Para imposto de renda
-           if(this.SalBruto <= 1.903){
+           if(this.SalBruto <= 1.903)
+           {
             this.SalLiq = SalBruto;
             this.ImpostoRenda = 0.0;
-           }else if(this.SalBruto >= 1.903 && this.SalBruto <= 2.826 ){
+           }else if(this.SalBruto >= 1.903 && this.SalBruto <= 2.826 )
+           {
             this.SalLiq = this.SalBruto - (this.SalBruto - 0.075);
             this.ImpostoRenda = 0.075;
-           }else if(this.SalBruto >= 2.826 && this.SalBruto <= 3.751){
+           }else if(this.SalBruto >= 2.826 && this.SalBruto <= 3.751)
+           {
             this.SalLiq = this.SalBruto - (this.SalBruto - 0.15);
             this.ImpostoRenda = 0.015;
-           }else if(this.SalBruto >= 3.751 && this.SalBruto <= 4664){
+           }else if(this.SalBruto >= 3.751 && this.SalBruto <= 4664)
+           {
                this.SalLiq = this.SalBruto - (this.SalBruto - 0.22);
                this.ImpostoRenda = 0.22;
            }else{
@@ -39,13 +43,16 @@ namespace API_Folhas.Models
            }
 
            //Para INSS
-           if(this.SalBruto <= 1.693){
+           if(this.SalBruto <= 1.693)
+           {
             this.SalLiq = this.SalBruto -( this.SalBruto * 0.08 );
             this.INSS = 0.08;
-           }else if(this.SalBruto >= 1.693 && this.SalBruto <= 2.822 ){            
+           }else if(this.SalBruto >= 1.693 && this.SalBruto <= 2.822 )
+           {            
             this.SalLiq = this.SalLiq - (this.SalLiq - 0.09);
             this.INSS = 0.09;
-           }else if(this.SalBruto >= 2.822 && this.SalBruto <= 5.645){
+           }else if(this.SalBruto >= 2.822 && this.SalBruto <= 5.645)
+           {
             this.SalLiq = this.SalLiq - (this.SalLiq - 0.11);
             this.INSS = 0.11;
            }else if(this.SalBruto > 5.645){
