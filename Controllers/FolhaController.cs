a36abaa61854cca725c15de6be252ade9943822d
FKRIK <fernandokrik@hotmail.com>
2022-10-06 22:09:07 -0300

    primeiro e unico commit

---
+++ Controllers/FolhaController.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using API_Folhas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Folhas.Controllers
+{
+    [ApiController]
+    [Route("api/folha")]
+    public class FolhaController : ControllerBase
+    {
+        private readonly DataContext _context;
+         public FolhaController(DataContext context) => _context = context;
+
+         // GET - Listar todas as folhas
+         // Rota - /api/folha/listar
+        [HttpGet]
+        [Route("listar")]
+        public IActionResult Listar()
+        {
+            return Ok(_context.Folhas.ToList());
+        }
+
+        // GET - Listar uma folha
+        // Rota: /api/folha/{cpf}/{mes}/{ano}
+        [HttpGet]
+        [Route("buscar/{cpf}/{mes}/{ano}")]
+        public IActionResult ListarUma([FromRoute] string cpf, string mes, string ano)
+        {
+            Folha folha = _context.Folhas.FirstOrDefault(a => a.Mes == mes && a.Ano == ano && a.CpfFunc == cpf);
+            Funcionario funcionaio = _context.Funcionarios.FirstOrDefault(f=> f.Cpf == folha.CpfFunc);
+            folha.funcionario = funcionaio;
+            return Ok(folha);
+        }
+
+
+        // GET - Listar folhas do mes
+        // Rota: /api/folha/{mes}/{ano}
+        [HttpGet]
+        [Route("filtrar/{mes}/{ano}")]
+        public IActionResult ListarMes(string mes, string ano)
+        {
+
+            List<Folha> folhas = new List<Folha>();
+            var busca = _context.Folhas.ToList().Where(f=> f.Mes.Equals(mes) && f.Ano.Equals(ano));
+                for(int i =0; i < busca.Count(); i++){
+                    busca.ElementAt(1).funcionario = _context.Funcionarios.FirstOrDefault(f=> f.Cpf == busca.ElementAt(i).CpfFunc);
+            }
+            return Ok(busca.ToList());
+        }
+        
+
+        // POST - Cadastrar uma folha
+        // Rota - /api/folha/cadastrar
+        [HttpPost]
+        [Route("cadastrar")]
+        public IActionResult Cadastrar([FromBody] Folha folha)
+        {
+           Funcionario funcionaio = _context.Funcionarios.FirstOrDefault(f=> f.Cpf == folha.CpfFunc);
+           Folha folha2 = new Folha(funcionaio);
+            _context.Folhas.Add(folha2);
+            _context.SaveChanges();
+            return Created("", folha2);
+        }
+    }
+}
---
+++ Models/Folha.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace API_Folhas.Models
+{
+    public class Folha
+    {
+        // Construtor vazio
+        public Folha(){ }
+        public int FolhaId { get; set; }
+        public string Ano {get; set;}
+        public string Mes {get; set;}
+        public string CpfFunc {get; set;}
+        public double ValorHora { get; set;}
+        public double QuantidaDedeHoras {get; set;}
+        public double SalBruto {get; set;}
+        public double SalLiq {get; set;}
+        public double ImpostoRenda {get; set;}
+        public double INSS {get; set;}
+        public double FGTS {get; set;}
+        public Funcionario funcionario {get; set;}
+        
+        public Folha(Funcionario funcionario){
+            // Para imposto de renda
+           if(this.SalBruto <= 1.903){
+            this.SalLiq = SalBruto;
+            this.ImpostoRenda = 0.0;
+           }else if(this.SalBruto >= 1.903 && this.SalBruto <= 2.826 ){
+            this.SalLiq = this.SalBruto - (this.SalBruto - 0.075);
+            this.ImpostoRenda = 0.075;
+           }else if(this.SalBruto >= 2.826 && this.SalBruto <= 3.751){
+            this.SalLiq = this.SalBruto - (this.SalBruto - 0.15);
+            this.ImpostoRenda = 0.015;
+           }else if(this.SalBruto >= 3.751 && this.SalBruto <= 4664){
+               this.SalLiq = this.SalBruto - (this.SalBruto - 0.22);
+               this.ImpostoRenda = 0.22;
+           }else{
+                this.SalLiq = this.SalBruto - (this.SalBruto - 0.27);
+                this.ImpostoRenda = 0.27;
+           }
+
+           //Para INSS
+           if(this.SalBruto <= 1.693){
+            this.SalLiq = this.SalBruto -( this.SalBruto * 0.08 );
+            this.INSS = 0.08;
+           }else if(this.SalBruto >= 1.693 && this.SalBruto <= 2.822 ){            
+            this.SalLiq = this.SalLiq - (this.SalLiq - 0.09);
+            this.INSS = 0.09;
+           }else if(this.SalBruto >= 2.822 && this.SalBruto <= 5.645){
+            this.SalLiq = this.SalLiq - (this.SalLiq - 0.11);
+            this.INSS = 0.11;
+           }else if(this.SalBruto > 5.645){
+               this.SalLiq = this.SalLiq - (this.SalLiq - 0.22);
+               this.INSS = 0.22;
+           }
+
+           //Para FGTS
+           this.SalLiq =  this.SalBruto - (this.SalBruto - 0.08);         
+           this.SalLiq = this.SalBruto - this.INSS - this.ImpostoRenda;
+        }
+        //public int FuncionarioId { get; set; }
+    }
+}
